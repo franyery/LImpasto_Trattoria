54d4eabceb77519833a5c15d59937da91067c071
nathalyhenriquez39 <nathalyhenriquez39@gmail.com>
2026-01-27 23:31:07 -0400

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# config.py universal
-SQLALCHEMY_DATABASE_URI = (
-    "mssql+pyodbc://./SISTEMA_DE_RESTAURANTE_TRATTORIA?" 
-    "driver=ODBC+Driver+17+for+SQL+Server&"
-    "trusted_connection=yes&"
-    "Encrypt=no&"
-    "TrustServerCertificate=yes"
-)
